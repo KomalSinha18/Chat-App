813d3f5103b4eefbb4dc0074703366afd7d7db57
komal <komalsinha1811@gmail.com>
2024-05-31 14:14:22 +0530

    changed socket link

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -38,7 +38,7 @@ const SignUp = () => {
             </label>
             <input
               type="text"
-              placeholder="Rohit Singh"
+              placeholder="Full Name"
               className="w-full input input-bordered h-10"
               value={inputs.fullName}
               onChange={(e) =>
@@ -52,7 +52,7 @@ const SignUp = () => {
             </label>
             <input
               type="text"
-              placeholder="Rohit Singh"
+              placeholder="Username"
               className="w-full input input-bordered h-10"
               value={inputs.username}
               onChange={(e) =>
